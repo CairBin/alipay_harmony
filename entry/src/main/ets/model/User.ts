c495d765d9e3e8829b53efa695be318c197c1831
rayer <1147627792@qq.com>
2024-09-10 10:46:31 +0800

    Realize bottom navigate

---
+++ entry/src/main/ets/model/User.ts
@@ -0,0 +1,11 @@
+export class User {
+  username: string
+  password: string
+  phone: number
+  constructor(username: string, password: string, phone: number) {
+    this.username = username
+    this.password = password
+    this.phone = phone
+  }
+
+}
